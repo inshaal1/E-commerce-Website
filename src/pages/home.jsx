d8c28350669d853c124073b488df2ab82b92a755
INSHAAL MOBEEN <salee.butt134@gmail.com>
2025-08-29 20:08:21 +0500

    adjusted some UI

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -19,21 +19,21 @@ const Home = () => {
   return (
     <div className="p-6">
       <h2 className="text-2xl font-bold mb-4">🔥 Hot Deals</h2>
-      <div className="flex gap-4 overflow-x-auto pb-2">
+      <div className="flex gap-3 overflow-x-auto pb-2">
         {discountItems.map((p) => (
           <div
             key={p.id}
-            className="border rounded-lg p-4 shadow min-w-[200px] flex-shrink-0"
+            className="border rounded-lg p-4 min-w-[100px] flex-shrink-0"
           >
             <img
               src={p.image}
               alt={p.name}
-              className="h-40 w-full object-cover"
+              className="h-40 w-full "
             />
             <h3 className="text-lg font-bold">{p.name}</h3>
-            <p className="line-through text-gray-500">${p.price}</p>
+            <p className="line-through text-gray-400">${p.price}</p>
             <p className="text-red-500 font-semibold">
-              ${p.discountPrice}
+              ${p.discountPrice}    
             </p>
             <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded">
               Add to Cart
@@ -46,16 +46,16 @@ const Home = () => {
       {Object.keys(categories).map((cat) => (
         <div key={cat} className="mb-8">
           <h3 className="text-xl font-semibold mb-3">{cat}</h3>
-          <div className="flex gap-4 overflow-x-auto pb-2">
+          <div className="flex gap-3 overflow-x-auto pb-2">
             {categories[cat].map((p) => (
               <div
                 key={p.id}
-                className="border rounded-lg p-4 shadow min-w-[200px] flex-shrink-0"
+                className="border rounded-lg p-4 min-w-[100px] flex-shrink-0"
               >
                 <img
                   src={p.image}
                   alt={p.name}
-                  className="h-40 w-full object-cover"
+                  className="h-40 w-full "
                 />
                 <h3 className="text-lg font-bold">{p.name}</h3>
                 <p>${p.price}</p>
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -4,8 +4,8 @@ const Products = () => {
   return (
    <div className="grid grid-cols-3 gap-4 p-6">
          {products.map((p) => (
-           <div key={p.id} className="border rounded-lg p-4 shadow">
-             <img src={p.image} alt={p.name} className="h-40 w-full object-cover" />
+           <div key={p.id} className="border rounded-lg p-4">
+             <img src={p.image} alt={p.name} className="h-40 w-full " />
              <h2 className="text-lg font-bold">{p.name}</h2>
              <p>${p.price}</p>
              <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded">
